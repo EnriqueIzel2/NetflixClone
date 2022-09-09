dd5df6f38bc2abd81431b09202cb0fb2b30b780d
EnriqueIzel2 <eleaoizel@gmail.com>
2022-09-08 21:00:06 -0400

    feat: create viewBinding

--- app/src/main/java/com/enrique/netflixclone/LoginActivity.kt
+++ app/src/main/java/com/enrique/netflixclone/LoginActivity.kt
@@ -2,11 +2,15 @@ package com.enrique.netflixclone
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.enrique.netflixclone.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
+  private lateinit var binding: ActivityLoginBinding
+
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
-    setContentView(R.layout.activity_login)
+    binding = ActivityLoginBinding.inflate(layoutInflater)
+    setContentView(binding.root)
 
     supportActionBar!!.hide()
   }
