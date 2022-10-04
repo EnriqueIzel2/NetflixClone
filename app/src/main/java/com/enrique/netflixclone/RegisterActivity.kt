d8af0a858504eae7f8722e7f929dc3711ae2bc7d
EnriqueIzel2 <eleaoizel@gmail.com>
2022-10-04 18:47:33 -0400

    feat: set binding at register

--- app/src/main/java/com/enrique/netflixclone/RegisterActivity.kt
+++ app/src/main/java/com/enrique/netflixclone/RegisterActivity.kt
@@ -2,10 +2,14 @@ package com.enrique.netflixclone
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.enrique.netflixclone.databinding.ActivityRegisterBinding
 
 class RegisterActivity : AppCompatActivity() {
+  private lateinit var binding: ActivityRegisterBinding
+
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
-    setContentView(R.layout.activity_register)
+    binding = ActivityRegisterBinding.inflate(layoutInflater)
+    setContentView(binding.root)
   }
 }
