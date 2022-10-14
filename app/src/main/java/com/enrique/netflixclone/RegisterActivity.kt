5b54a73926e295e244dce8d1aa0bbd33460601d6
EnriqueIzel2 <eleaoizel@gmail.com>
2022-10-13 20:49:01 -0400

    feat: create method to register user

--- app/src/main/java/com/enrique/netflixclone/RegisterActivity.kt
+++ app/src/main/java/com/enrique/netflixclone/RegisterActivity.kt
@@ -2,7 +2,9 @@ package com.enrique.netflixclone
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.enrique.netflixclone.databinding.ActivityRegisterBinding
+import com.google.firebase.auth.FirebaseAuth
 
 class RegisterActivity : AppCompatActivity() {
   private lateinit var binding: ActivityRegisterBinding
@@ -22,8 +24,27 @@ class RegisterActivity : AppCompatActivity() {
 
       if (email.isEmpty() || password.isEmpty()) {
         regErrorMessage.setText("Preencha todos os campos")
+      } else {
+        registerUser()
       }
     }
+  }
+
+  private fun registerUser() {
+    val email = binding.editRegisterEmail.text.toString()
+    val password = binding.editRegisterPassword.text.toString()
+    val infoMessage = binding.registerErroMessage
+
+    FirebaseAuth.getInstance().createUserWithEmailAndPassword(email, password)
+      .addOnCompleteListener {
+        if (it.isSuccessful) {
+          Toast.makeText(this, "Usuário cadastrado com sucesso", Toast.LENGTH_SHORT).show()
+          binding.editRegisterEmail.setText("")
+          binding.editRegisterPassword.setText("")
+        }
+      }.addOnFailureListener {
+        infoMessage.setText("Erro ao cadastrar usuário")
+      }
   }
 
   private fun setToolBar() {
