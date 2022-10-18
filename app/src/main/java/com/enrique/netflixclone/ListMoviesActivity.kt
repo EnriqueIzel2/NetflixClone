72fa38d7c176504e2c274ca3e4134951c66098e1
EnriqueIzel2 <eleaoizel@gmail.com>
2022-10-17 20:13:28 -0400

    feat: create method to logout user

--- app/src/main/java/com/enrique/netflixclone/ListMoviesActivity.kt
+++ app/src/main/java/com/enrique/netflixclone/ListMoviesActivity.kt
@@ -3,7 +3,9 @@ package com.enrique.netflixclone
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
+import android.view.MenuItem
 import com.enrique.netflixclone.databinding.ActivityListMoviesBinding
+import com.google.firebase.auth.FirebaseAuth
 
 class ListMoviesActivity : AppCompatActivity() {
   private lateinit var binding: ActivityListMoviesBinding
@@ -12,13 +14,21 @@ class ListMoviesActivity : AppCompatActivity() {
     super.onCreate(savedInstanceState)
     binding = ActivityListMoviesBinding.inflate(layoutInflater)
     setContentView(binding.root)
-
-
   }
 
   override fun onCreateOptionsMenu(menu: Menu): Boolean {
     val inflate = menuInflater
     inflate.inflate(R.menu.main_menu, menu)
     return true
+  }
+
+  override fun onOptionsItemSelected(item: MenuItem): Boolean {
+    when (item.itemId) {
+      R.id.menu_logout -> {
+        FirebaseAuth.getInstance().signOut()
+      }
+    }
+
+    return super.onOptionsItemSelected(item)
   }
 }
