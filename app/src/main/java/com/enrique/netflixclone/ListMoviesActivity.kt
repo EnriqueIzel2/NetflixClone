b461dc3fdc558b2615f21cb9b43a44165d411cd5
EnriqueIzel2 <eleaoizel@gmail.com>
2022-10-17 20:07:33 -0400

    feat: inflate the menu on options menu

--- app/src/main/java/com/enrique/netflixclone/ListMoviesActivity.kt
+++ app/src/main/java/com/enrique/netflixclone/ListMoviesActivity.kt
@@ -2,6 +2,7 @@ package com.enrique.netflixclone
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
 import com.enrique.netflixclone.databinding.ActivityListMoviesBinding
 
 class ListMoviesActivity : AppCompatActivity() {
@@ -11,5 +12,13 @@ class ListMoviesActivity : AppCompatActivity() {
     super.onCreate(savedInstanceState)
     binding = ActivityListMoviesBinding.inflate(layoutInflater)
     setContentView(binding.root)
+
+
+  }
+
+  override fun onCreateOptionsMenu(menu: Menu): Boolean {
+    val inflate = menuInflater
+    inflate.inflate(R.menu.main_menu, menu)
+    return true
   }
 }
