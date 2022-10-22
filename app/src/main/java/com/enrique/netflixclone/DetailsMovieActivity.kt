b0599b711b464465ed3526cb103f620c01a11e72
EnriqueIzel2 <eleaoizel@gmail.com>
2022-10-22 10:56:53 -0400

    feat: set binding

--- app/src/main/java/com/enrique/netflixclone/DetailsMovieActivity.kt
+++ app/src/main/java/com/enrique/netflixclone/DetailsMovieActivity.kt
@@ -2,10 +2,14 @@ package com.enrique.netflixclone
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.enrique.netflixclone.databinding.ActivityDetailsMovieBinding
 
 class DetailsMovieActivity : AppCompatActivity() {
+  private lateinit var binding: ActivityDetailsMovieBinding
+
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
-    setContentView(R.layout.activity_details_movie)
+    binding = ActivityDetailsMovieBinding.inflate(layoutInflater)
+    setContentView(binding.root)
   }
 }
