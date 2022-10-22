e49c88f820be8255d5f69cc90cf7777fd10da4a9
EnriqueIzel2 <eleaoizel@gmail.com>
2022-10-22 11:30:08 -0400

    feat: create a toolbar manage

--- app/src/main/java/com/enrique/netflixclone/DetailsMovieActivity.kt
+++ app/src/main/java/com/enrique/netflixclone/DetailsMovieActivity.kt
@@ -1,8 +1,12 @@
 package com.enrique.netflixclone
 
+import adapter.MoviesAdapter
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.GridLayoutManager
 import com.enrique.netflixclone.databinding.ActivityDetailsMovieBinding
+import model.addMovies
 
 class DetailsMovieActivity : AppCompatActivity() {
   private lateinit var binding: ActivityDetailsMovieBinding
@@ -13,6 +17,18 @@ class DetailsMovieActivity : AppCompatActivity() {
     setContentView(binding.root)
 
     supportActionBar!!.hide()
+    manageToolbal()
 
+    val recyclerMoreMovies = binding.detailsMovieMoreMovies
+    recyclerMoreMovies.adapter = MoviesAdapter(addMovies())
+    recyclerMoreMovies.layoutManager = GridLayoutManager(applicationContext, 3)
+  }
+
+  private fun manageToolbal() {
+    val toolbarDetails = binding.toolbarDetails
+    toolbarDetails.setNavigationIcon(getDrawable(R.drawable.ic_arrow_back_24))
+    toolbarDetails.setNavigationOnClickListener {
+      finish()
+    }
   }
 }
